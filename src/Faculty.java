742df5641df1468994433df4e4c675e7076ffe9a
Oleksii Chernikov <chernikov.lesha@gmail.com>
2014-03-01 18:51:57 +0200

    General structure.

---
+++ src/Faculty.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Alexey
+ * At 6:48 PM on 3/1/14
+ */
+
+public class Faculty {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Alexey
  * At 6:29 PM on 3/1/14
  */
 
 public class Main {
-    public static void main(String[] args) {
+    private ArrayList<String> commands = new ArrayList<String>(){{
+        add("addfac");
+        add("editfac");
+        add("delfac");
+    }};
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isCommand = true;
+        while(scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+        }
     }
 }
